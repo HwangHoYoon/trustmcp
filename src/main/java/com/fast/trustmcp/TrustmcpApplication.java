a90da14f316e3fd7002a1df18f5293c40bc256c3
황호윤 <hoyoon9227@gmail.com>
2026-02-01 02:18:17 +0900

    mcp 소스 수정

--- src/main/java/com/fast/trustmcp/TrustmcpApplication.java
+++ src/main/java/com/fast/trustmcp/TrustmcpApplication.java
@@ -1,32 +1,22 @@
 package com.fast.trustmcp;
 
-import com.fast.trustmcp.tools.FileSystemTools;
 import com.fast.trustmcp.tools.ScanTools;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class TrustmcpApplication {
-
-    @Autowired
-    private FileSystemTools fileSystemTools;
-
-    @Autowired
-    private ScanTools scanTools;
 
     public static void main(String[] args) {
         SpringApplication.run(TrustmcpApplication.class, args);
     }
-
     @Bean
-    public ToolCallbackProvider getToolCallbackProvider() {
-        return MethodToolCallbackProvider.builder()
-                .toolObjects(
-                        fileSystemTools, scanTools)
-                .build();
+    public List<ToolCallback> danTools(ScanTools scanTools) {
+        return List.of(ToolCallbacks.from(scanTools));
     }
 }
